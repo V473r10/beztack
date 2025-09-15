effd1c6dd95ba0415b645bdf11d092e1e71f3eb9
V473R10 <punk.road2928@codedicated.com>
2025-09-14 22:33:44 -0300

    remove .js files wtf

--- packages/ocr/index.js
+++
@@ -1,7 +0,0 @@
-import { createWorker } from "tesseract.js";
-export const extractTextFromImage = async (imagePath, lang) => {
-  const worker = await createWorker(lang);
-  const ret = await worker.recognize(imagePath);
-  await worker.terminate();
-  return ret.data.text;
-};
