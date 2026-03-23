5548f6157391c96677d3589a1aa03be09cf3175f
Facundo Valerio <fac.valerio@gmail.com>
2026-03-23 00:38:03 -0300

    refactor: extract HTTP redirect status code to named constant in success route handler
    - Changed hardcoded 302 status code to REDIRECT_STATUS constant in sendRedirect call

--- apps/api/server/routes/success.get.ts
+++ apps/api/server/routes/success.get.ts
@@ -1,6 +1,7 @@
 import { defineEventHandler, sendRedirect } from "h3";
 
+const REDIRECT_STATUS = 302;
 export default defineEventHandler((event) => {
   // Redirect to the success pag
-  sendRedirect(event, "http://localhost:5173/", 302);
+  sendRedirect(event, "http://localhost:5173/", REDIRECT_STATUS);
 });
